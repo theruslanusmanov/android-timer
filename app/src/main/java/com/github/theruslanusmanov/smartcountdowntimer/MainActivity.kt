78a6f3b486a621e34d8aae1b18728dc807559c17
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-06-12 12:35:18 +0500

    add basic layout for timer

--- app/src/main/java/com/github/theruslanusmanov/smartcountdowntimer/MainActivity.kt
+++ app/src/main/java/com/github/theruslanusmanov/smartcountdowntimer/MainActivity.kt
@@ -3,44 +3,60 @@ package com.github.theruslanusmanov.smartcountdowntimer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "jetpack Compose"))
+            Timer()
         }
     }
 }
 
-data class Message(val author: String, val body: String)
+@Composable
+fun MessageCard(msg: String) {
+    Text(
+        text = msg,
+        fontWeight = FontWeight.Bold,
+        fontSize = 96.sp
+    )
+}
 
 @Composable
-fun MessageCard(msg: Message) {
-    // Add padding around our message
-    Row(modifier = Modifier.padding(all = 8.dp)) {
-        Column {
-            Text(text = msg.author)
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
-        }
+fun Input() {
+    OutlinedTextField(
+        value = "10 m",
+        onValueChange = {},
+        Modifier.background(Color.LightGray),
+        textStyle = TextStyle(fontWeight = FontWeight.Bold)
+    )
+}
+
+@Composable
+fun Timer() {
+    Column(Modifier.padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+        MessageCard("10:33")
+        Spacer(modifier = Modifier.height(4.dp))
+        Input()
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
-    )
+fun Preview() {
+    Timer();
 }
