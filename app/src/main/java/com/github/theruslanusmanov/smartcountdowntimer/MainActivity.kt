3c27a1e08e07484e1ff1c5deb918e776dd296157
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-06-12 09:17:23 +0500

    add text

--- app/src/main/java/com/github/theruslanusmanov/smartcountdowntimer/MainActivity.kt
+++ app/src/main/java/com/github/theruslanusmanov/smartcountdowntimer/MainActivity.kt
@@ -3,41 +3,26 @@ package com.github.theruslanusmanov.smartcountdowntimer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.github.theruslanusmanov.smartcountdowntimer.ui.theme.SmartCountdownTimerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SmartCountdownTimerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Text("Hello world!")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
+fun MessageCard(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    SmartCountdownTimerTheme {
-        Greeting("Android")
-    }
+fun PreviewMessageCard() {
+    MessageCard("Android")
 }
