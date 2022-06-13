f16370e73dd0bf85d652c9fc0f2ba9dbfbaed385
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-06-13 09:04:53 +0500

    add time picker

--- app/src/main/java/com/github/theruslanusmanov/smartcountdowntimer/MainActivity.kt
+++ app/src/main/java/com/github/theruslanusmanov/smartcountdowntimer/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.github.theruslanusmanov.smartcountdowntimer
 
+import android.app.TimePickerDialog
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -16,24 +21,54 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import java.util.*
 
 // TODO: Use Jchronic or Natty to parse time
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Timer()
+            Timer(this)
         }
     }
 }
 
 @Composable
-fun MessageCard(msg: String) {
+fun TimeView(msg: String) {
     Text(
         text = msg,
         fontWeight = FontWeight.Bold,
         fontSize = 96.sp
     )
+}
+
+@Composable
+fun TimePicker(context: Context) {
+    val calendar = Calendar.getInstance()
+    val hour = calendar[Calendar.HOUR_OF_DAY]
+    val minute = calendar[Calendar.MINUTE]
+
+    val time = remember { mutableStateOf("") }
+    val timePickerDialog = TimePickerDialog(
+        context,
+        { _, hour: Int, minute: Int ->
+            time.value = "$hour:$minute"
+        }, hour, minute, false
+    )
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Selected Time: ${time.value}")
+        Spacer(modifier = Modifier.size(16.dp))
+        Button(onClick = {
+            timePickerDialog.show()
+        }) {
+            Text(text = "Open Time Picker")
+        }
+    }
 }
 
 @Composable
@@ -47,16 +82,17 @@ fun Input() {
 }
 
 @Composable
-fun Timer() {
+fun Timer(ctx: Context) {
     Column(
         Modifier
             .padding(8.dp)
             .fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        MessageCard("10:33")
+        TimeView("10:33")
         Spacer(modifier = Modifier.height(4.dp))
-        Input()
+        // Input()
+        TimePicker(context = ctx)
     }
 }
 
